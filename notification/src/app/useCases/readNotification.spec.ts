f50804d403bfe0dbd832eb458a8941154ce08354
eduardosdl <edu.silva.e08@gmail.com>
2023-01-25 11:32:47 -0300

    test use case read notification

---
+++ notification/src/app/useCases/readNotification.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notificationFactory';
+import { InMemoryNotificationRepository } from '@test/repositories/inMemoryNotificationRepository';
+import { ReadNotification } from './readNotification';
+import { NotificationNotFound } from './errors/notificationNotFound';
+
+describe('Read notification', () => {
+  it('Should be able to read a notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const readNotification = new ReadNotification(notificationRepository);
+
+    const notification = makeNotification();
+
+    await notificationRepository.create(notification);
+
+    await readNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationRepository.notifications[0].readAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to read a non existing notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const readNotification = new ReadNotification(notificationRepository);
+
+    expect(() => {
+      return readNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});

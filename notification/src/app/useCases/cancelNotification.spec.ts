ac274f41eb6e80c1a60b8acd0930d81643d4e157
eduardosdl <edu.silva.e08@gmail.com>
2023-01-27 10:13:31 -0300

    test cancel notification

---
+++ notification/src/app/useCases/cancelNotification.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notificationFactory';
+import { InMemoryNotificationRepository } from '@test/repositories/inMemoryNotificationRepository';
+import { CancelNotification } from './cancelNotification';
+import { NotificationNotFound } from './errors/notificationNotFound';
+
+describe('Cancel notification', () => {
+  it('Should be able to cancel a notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    const notification = makeNotification();
+
+    await notificationRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', async () => {
+    const notificationRepository = new InMemoryNotificationRepository();
+    const cancelNotification = new CancelNotification(notificationRepository);
+
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
